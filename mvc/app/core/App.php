d34393a5bd7508b72686841c0ad6369199420cb1
lequihieu <toatugiap3@gmail.com>
2020-06-11 23:12:35 +0700

    first commit

---
+++ mvc/app/core/App.php
@@ -0,0 +1,22 @@
+<?php
+
+class App {
+
+    protected $controller = 'home';
+
+    protected $method = 'index';
+
+    protected $params = [];
+
+    public function __construct()
+    {
+        $this->parseUrl;
+    }
+
+    public function parseUrl()
+    {
+        if(isset($_GET['url'])) {
+            echo $_GET['url'];
+        }
+    }
+}
