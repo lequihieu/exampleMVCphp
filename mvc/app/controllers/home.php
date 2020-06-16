68a4592ef6c0c75c81de5296a444e50319d30ef7
hieu <toatugiap3@gmail.com>
2020-06-16 20:50:09 +0700

    add crud

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -21,21 +21,67 @@ class Home extends Controller
     }
 
     public function addStudent() {
-        header('Content-type: text/javascript');
-        $json = array(
-            'name' => '',
-            'age'  => '',
-            'email' => '',
-            'class' => '',
-            'success' => false
-        );
+        $servername = "localhost";
+        $username = "root";
+        $password = "Ridaica123";
+        $dbname = "studentdb";
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-            $json['name'] = $_POST['name'];
-            $json['age'] = $_POST['age'];
-            $json['email'] = $_POST['email'];
-            $json['class'] = $_POST['class'];
-            $json['success'] = true;
+
+        //header('Content-type: text/javascript');
+        // $json = array(
+        //     'name' => '',
+        //     'age'  => '',
+        //     'email' => '',
+        //     'class' => '',
+        //     'success' => false
+        // );
+
+        //     $json['name'] = $_POST['name'];
+        //     $json['age'] = $_POST['age'];
+        //     $json['email'] = $_POST['email'];
+        //     $json['class'] = $_POST['class'];
+        //     $json['success'] = true;
+       
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
         
-        echo "abc";
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $email = $_POST['email'];
+        $class = $_POST['class'];
+        
+        $sql = 'INSERT INTO student(name, age, email, class) VALUE('."'".$name."',"."'".$age."','".$email."','".$class."')";
+       
+        $result = mysqli_query($conn, $sql);
+
+
+        echo $result;
+    }
+
+    public function getAllList() {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "Ridaica123";
+        $dbname = "studentdb";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT * FROM student");
+        $stmt->execute();
+      
+        // set the resulting array to associative
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+        
+    }
+
+    public function deleteStudent() {
+
+    }
+
+    public function UpdateStudent() {
+      
     }
 }
--- mvc/app/views/home/formStudent.php
+++ mvc/app/views/home/formStudent.php
@@ -6,11 +6,13 @@
   </style>
   <title></title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.css">
 </head>
 <body>
 <div class="container">
-  <div class="row">
-    <div class="col-3">
+    <div class="row">
+        <div class="col-3">
               <div>
                       <form id = "idForm" class="contact-form" method="post" action = "../home/addStudent">
                               <div class="form-group">
@@ -18,7 +20,7 @@
                                       <span class="error">* <?php //echo $nameErr?></span>
                               </div>
                               <div class="form-group">
-                                      <input class="form-control" type="text" name="phone" placeholder="Age">
+                                      <input class="form-control" type="text" name="age" placeholder="Age">
                                       <span class="error">* <?php //echo $phoneErr?></span>
                               </div>
                               <div class="form-group">
@@ -33,9 +35,23 @@
                       
                       </form>
               </div>
-    </div>
-    <div class="col-9">
+     </div>
+      <div class="col-9">
+
+                  <div>
+                    <form  id = "idGetAll" method="get" action = "../home/getAllList">
+                      <button class="btn btn-primary" type="submit" name="getAllList" >Get All Student </button>
+                    </form>
+                  </div>
+
+                  <div id = "allList">
                   
+                  </div>
+
+                  <div>
+                      
+                  </div>
+      </div>    
     </div>   
 </div>  
 <script
@@ -44,6 +60,8 @@
   crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.js"></script>
     <script type="text/javascript">
       
       $("#idForm").submit(function(e) {
@@ -52,22 +70,90 @@
 
         var form = $(this);
         var url = form.attr('action');
-
+        
         $.ajax({
             type: "POST",
             url: url,
-            data: form.serialize() // serializes the form's elements.
+            data: form.serialize(),  // serializes the form's elements.
             success: function(data)
             {
               
-                  alert(data);
-               
+                  
+                  alert(data);                
                   
                  // show response from the php script.
             }
         });
+      });
 
+      // function load_ajax()
+      //       {   
+      //           $.ajax({
+      //               url : "../home/renderAllList",
+      //               type : "get", 
+      //               dateType:"text", // dữ liệu trả về dạng text
+                    
+      //               success : function (result){
+                        
+      //                   $('#result').html(result);
+      //               }
+      //           });
+      //       } 
+      $("#idGetAll").submit(function(e) {
 
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+
+        var form = $(this);
+        var url = form.attr('action');
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function(data)
+            {
+              var dataObj = JSON.parse(data);    
+              
+                var view = "";
+                if(dataObj.length > 0) 
+                {
+                  view = '<table class="table">' +
+                            '<thead>' +
+                              '<tr> ' +
+                              '<th scope="col">Id</th>' +
+                              '<th scope="col">Name</th>' +
+                              '<th scope="col">Age</th>' +
+                              '<th scope="col">Email</th>' +
+                              '<th scope="col">Class</th>' +
+                              '</tr>' +
+                            '</thead>' +
+                            '<tbody>';
+                  
+                  for(var i = 0; i < dataObj.length; i++) {
+
+                    view =  view + '<tr>' +
+                        '<th scope="row">' + dataObj[i].id +'</th>' +
+                        '<td>' + dataObj[i].name + '</td>' +
+                        '<td>' + dataObj[i].age + '</td>' +
+                        '<td>' + dataObj[i].email + '</td>' +
+                        '<td>' + dataObj[i].class + '</td>' +
+                        '<td>' +
+                        '<form method="post">' +
+                        '<input type="hidden" name="id" value=' + dataObj[i].id + '>' +
+                        '<input class="btn btn-primary" type="submit" name="fillToUpdate" value="Update"' + 'onclick="return confirm(' + "'Want to Update?'" + ')"> ' +
+                        '<input class="btn btn-primary" type="submit" name="deleteById" value="Delete"' + 'onclick="return confirm(' + "'Want to Delete?'" + ')">' +
+                        // onclick="return confirm('Want to Submit?')
+                        '</form>' +
+                        '</td>' +
+                      '</tr>'; 
+                  }
+                  view = view + '</tbody></table>';
+         
+                  
+                }
+                document.getElementById("allList").innerHTML = view;
+
+            }                             
+        });
       });
     </script>
     
